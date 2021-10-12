a7fefde68b4b0013d36a2f5bae4814ba204a4bd6
Daniella Alamo <dalamov08@gmail.com>
2021-10-12 16:26:51 +0200

    make the wrapperComponent function and delete the components in new product

--- src/pages/NewProduct/NewProduct.js
+++ src/pages/NewProduct/NewProduct.js
@@ -14,28 +14,9 @@ import NewProductForm from "../../components/NewProductForm";
  */
 function NewProduct({ saveNewProduct }) {
   return (
-    <>
-      <AppHeader />
-      <Main className="container">
-        <div className="row justify-content-center">
-          <div className="col col-8">
-            <div className="row">
-              <header className="col col-12">
-                <h1>New product</h1>
-              </header>
-              <div className="col col-12">
-                <hr />
-              </div>
-
-              <div className="col col-12">
-                <NewProductForm saveNewProduct={saveNewProduct} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </Main>
-      <Footer />
-    </>
+    <div className="col col-12">
+      <NewProductForm saveNewProduct={saveNewProduct} />
+    </div>
   );
 }
 
